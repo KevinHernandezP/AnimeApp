7b1113fdd05c623ecadcfc6058a71ed775e4fc50
Kevin Hernandez <kevinmusicmc@gmail.com>
2021-05-06 08:43:08 -0500

    Quinto commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hice cambio en la app
+          wanda
         </a>
       </header>
     </div>
