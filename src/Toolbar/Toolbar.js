fccf9641bea0b026e01d0755344fd3fb63c14165
Kevin Hernandez <kevinmusicmc@gmail.com>
2021-05-08 06:15:21 -0500

    changes in navbar and the size of home

--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -6,17 +6,17 @@ import { NavDropdown,Navbar,Nav } from 'react-bootstrap';
 const Toolbar = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg"  className="dark">
-                <Navbar.Brand href="#home" className="p-2 text-white fw-bold m-1">AnimeApp</Navbar.Brand>
+            <Navbar collapseOnSelect expand="lg"  className="dark px-4">
+                <Navbar.Brand href="#home" className="py-1 text-white fw-bold logo px-5">AnimeApp</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="m-auto ">
                    
                         </Nav>
-                    <Nav className="py-1">
-                        <Nav.Link href="#deets" className="text-white texto" style={{color:"blue"}}>Home</Nav.Link>
+                    <Nav className="py-1 p-3 p-4">
+                        <Nav.Link href="#deets" className="text-white texto " style={{color:"blue"}}>Home</Nav.Link>
                         <Nav.Link href="#deets" className="text-white texto btn-custom">Animes</Nav.Link>
-                        <Nav.Link href="#deets" className="text-white texto">Contact us</Nav.Link>
+                        <Nav.Link href="#deets" className="text-white texto">Contact</Nav.Link>
 
                     </Nav>
                 </Navbar.Collapse>
